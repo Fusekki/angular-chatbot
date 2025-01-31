c986f16fe1d7d6998dd1f216f831c70f2d0549f4
Phillip Stafford <tacosnakk@gmail.com>
2025-01-30 21:14:28 -0500

    remove builder. switch back to environment file

--- src/app/services/apiservice/apiservice.service.ts
+++ src/app/services/apiservice/apiservice.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { timer } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +11,7 @@ export class ApiService {
   GEMINI_API_KEY: string = '';
 
   constructor() {
-    this.GEMINI_API_KEY = import.meta.env['NG_APP_GEMINI_API_KEY'];
-    console.log('key: ', this.GEMINI_API_KEY)
+    this.GEMINI_API_KEY = environment.GEMINI_API_KEY;
     this.genAI = new GoogleGenerativeAI(this.GEMINI_API_KEY);
     this.model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: false
+  production: false,
+  GEMINI_API_KEY: 'AIzaSyARC2-FW51IqCb3FHaBYEThjc7DwMk6qBc',
+  PROJECT_ID: 'proj_y6k6MHScrROFNuQLpxe6FZVo'
 };
